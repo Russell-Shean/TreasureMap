1cc5b579eac41e7178ba2146aa2cbe54dc56d935
Russell Shean <73149125+Russell-Shean@users.noreply.github.com>
2021-07-16 15:14:59 +0800

    Add files via upload

--- Treasure map github version.R
+++ Treasure map github version.R
@@ -1,20 +1,16 @@
 # Treasure map
-
-# This is the code for: 
-# If I gave you a version of the map and you're looking at the source code,...
-# Sorrrrryy, I changed the password and the coordinates for the git hub version
-# Guess the only way to find the treasure is by following the clues! 
-# If you're curious about how I made the map, here's the code:
-
+# This is a modified version of a treasure map I made for Taipei
+# The original map actually leads to buried treasure!
+# This map doesn't lead anywhere. It just shows how the map was made. 
 
 library(leaflet)
 library(shiny)
 library(dplyr)
 
 ui <- fluidPage(
-  fluidRow(column(4,textInput("password","Thar fiRRRst passwarrrd?")),
-           column(4,textInput("password2","Thar 2nd passwarrrd?"))),
-  fluidRow(leafletOutput("trsrrrmap"))
+  fluidRow(column(4,textInput("password","The first password?")),
+           column(4,textInput("password2","The second password?"))),
+  fluidRow(leafletOutput("TreasureMap"))
 )
 
 # code for treasure map x: https://rud.is/b/2015/09/19/a-package-full-o-pirates-makin-interactive-pirate-maps-in-arrrrrrstats/
@@ -24,7 +20,7 @@ server<- function(input,output,session){
                     iconHeight=16, iconWidth=16,
                     iconAnchorX=8, iconAnchorY=8)
   
-  output$trsrrrmap <- renderLeaflet(leaflet()%>%
+  output$TreasureMap <- renderLeaflet(leaflet()%>%
                                       addProviderTiles(providers$Stamen.Watercolor)%>%
                                       addMarkers(icon=x_marker,lat=25.034573001368337, 
                                                  lng=121.51925971356991,
@@ -37,7 +33,7 @@ server<- function(input,output,session){
                                                             129),
                                                  popup = ifelse(input$password=="password1",
                                                                 "clue1",
-                                                                "sorry wrong passwarrrrd no TreasuRRRR for you"))%>%
+                                                                "Sorry, you haven't found the right password yet"))%>%
                                       addMarkers(icon=x_marker,lat=ifelse(input$password2=="password2",
                                                                           25, 
                                                                           21),
@@ -46,7 +42,7 @@ server<- function(input,output,session){
                                                             110),
                                                  popup  = ifelse(input$password2=="password2",
                                                                  "clue2",
-                                                                 "Tharrr 2nd passwarrrrds wrong!"))
+                                                                 "Sorry, you haven't found the right password yet"))
                                     
   )
 }
